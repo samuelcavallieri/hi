e43159fc6ff1acc161145cd645ef950ca54c15b5
samuel-cavallieri <samuel.cavallieri@schibsted.com>
2022-06-18 02:04:55 +0200

    working in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,4 @@ def test_send():
     response = client.post("/send",json={"message": "Hello there"})
     assert response.status_code == 201
     assert response.json() == {"msg": "Hello there"}
+    assert 404 == 201

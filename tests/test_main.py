0eb4645a8d42ce7285727d85a64057e5cb6079f8
samuel-cavallieri <samuel.cavallieri@schibsted.com>
2022-06-20 23:24:16 +0200

    change msg to message

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_send():
     response = client.post("/send",json={"message": "Hello there"})
     assert response.status_code == 201
-    assert response.json() == {"msg": "Hello there"}
+    assert response.json() == {"message": "Hello there"}
